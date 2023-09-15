6a90446d9d324fed38e32a253e33e8fc0b8f71e2
panicboat <panicboat@gmail.com>
2023-09-15 15:37:49 +0900

    Delete locust

--- locust/README.md
+++
@@ -1,1 +0,0 @@
-docker-compose up --scale worker=4
--- locust/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 2)
-
-    @task
-    def index(self):
-        self.client.get("/")
