8285e1631343314863a8d18dbdd99d5319ebe586
panicboat <panicboat@gmail.com>
2024-05-21 16:07:55 +0900

    Create git-switch.sh

---
+++ bash/git-switch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# set -eu
+
+working_dir="$(cd $(dirname $0);pwd)"
+list=($(find $working_dir -mindepth 1 -maxdepth 1 -type d))
+
+for i in "${!list[@]}"
+do
+  echo "$i => ${list[$i]}"
+  cd ${list[$i]}
+  git switch $(git symbolic-ref refs/remotes/origin/HEAD | cut -f4 -d'/')
+  git pull origin
+done
